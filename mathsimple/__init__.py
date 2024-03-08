afdb42245596903a753cb7cf8b9208ef9d7a5051
szzin20 <92289030+szzin20@users.noreply.github.com>
2024-03-08 20:06:10 +0700

    feat: add simple calculator

---
+++ mathsimple/__init__.py
@@ -0,0 +1,4 @@
+from . import geometry
+from . import operations
+
+__all__ = ['geometry', 'operations']
--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Simple Math'
+LONG_DESCRIPTION = 'A simple Python package for math operations and geometry calculations'
+
+# Setting up
 setup(
-name="math-simple",
-version="0.1.0",
-description="A simple Python package for math operations and geometry calculations",
-packages=find_packages(),
-classifiers=[
-"Programming Language :: Python :: 3",
-"License :: OSI Approved :: MIT License",
-"Operating System :: OS Independent",
-],
-python_requires=">=3.6",
+    name="simplemath",
+    version=VERSION,
+    author="Ikhtiar Oktafio Wibowo",
+    author_email="tiarokta@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    keywords=['python', 'math', 'simple'],
+    license="MIT",
+    url="https://github.com/szzin20/simple-math.git",
+    project_urls={
+        'Source': 'https://github.com/szzin20/simple-math.git',
+    },
 )
